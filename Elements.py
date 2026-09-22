c998bc86de816be8e3bbd06aa69eb8729fbe8300
GoldenDraagonJD <lisbe2003@gmail.com>
2026-09-22 16:29:06 -0400

    Created a parent class called Element for the sub elements so if I need to add any new variables I can add it to all of them with the parent class

--- Elements.py
+++ Elements.py
@@ -17,7 +17,6 @@ class ProgressBar(Element):
         self.locations = [(location_x + i, location_y) for i in range(size)]
         self.progress = 0
         self.total_progress = 100
-        self.is_interactive = False
 
     def change_progress(self, progress):
         self.progress = progress
@@ -36,14 +35,11 @@ class ProgressBar(Element):
             self.ui.grid[loc[1]][loc[0]] = current_progress[i]
 
 
-class Label:
+class Label(Element):
     def __init__(self, uiBuilder: UIBuilder, location_x, location_y, text):
-        self.ui = uiBuilder
-        self.location_x = location_x
-        self.location_y = location_y
+        super().__init__(uiBuilder, location_x, location_y)
         self.text = text
         self.max_length = self.ui.grid_size_x - location_x - 1
-        self.is_interactive = False
 
     def add_to_grid(self):
         self.text = UIBuilder.truncate_text(self.text, self.max_length)
@@ -51,10 +47,8 @@ class Label:
             self.ui.grid[self.location_y][self.location_x + i] = char
 
 
-class Input:
+class Input(Element):
     def __init__(self, uiBuilder: UIBuilder, location_x, location_y) -> None:
-        self.ui = uiBuilder
-        self.location_x = location_x
-        self.location_y = location_y
+        super().__init__(uiBuilder, location_x, location_y)
         self.placeholder = ""
         self.is_interactive = True
